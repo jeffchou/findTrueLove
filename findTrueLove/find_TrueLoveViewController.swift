367fc1d37c1e43cefc6aef248676b8c298919ef8
ShihChieh Chou <jeffchou@shihchiehchoudemacbook-air.local>
2016-10-24 11:09:48 +0800

    Initial Commit

---
+++ findTrueLove/find_TrueLoveViewController.swift
@@ -0,0 +1,61 @@
+//
+//  find_TrueLoveViewController.swift
+//  findTrueLove
+//
+//  Created by ShihChiehChou on 10/24/16.
+//  Copyright © 2016 Jeff Chou. All rights reserved.
+//
+
+import UIKit
+
+class find_TrueLoveViewController: UIViewController {
+
+    @IBOutlet weak var constellationTextField: UITextField!
+    @IBOutlet weak var sexSegctl: UISegmentedControl!
+    @IBOutlet weak var trueSwitch: UISwitch!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var resultSegctl: UISegmentedControl!
+    
+    @IBAction func findTrueLove(_ sender: AnyObject) {
+        let constellation = constellationTextField.text!
+        let sex = sexSegctl.selectedSegmentIndex
+        let trueLove = trueSwitch.isOn
+        let age = Int(ageLabel.text!)
+        resultSegctl.isHidden = true
+        
+        if constellation == "Aries" && sex == 1 && trueLove && age! <= 35 && age! >= 26
+        {
+            resultSegctl.selectedSegmentIndex = 0
+            resultSegctl.isHidden = false
+        }
+        else
+        {
+            resultSegctl.selectedSegmentIndex = 1
+            resultSegctl.isHidden = false
+
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
